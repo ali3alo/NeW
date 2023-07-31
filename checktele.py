0d8d611b9f362d1b86b32329dd0db15f33ec56d3
ali3alo <120683286+ali3alo@users.noreply.github.com>
2023-07-31 19:15:21 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -241,19 +241,19 @@ def gen_user(choice):
             f = [c[0], d[0], "_", d[0], d[0], d[0]]
             username = ''.join(f)
         else:
-             pass
+            pass
      if choice == "15":
         c = random.choices(a)
         d = random.choices(a) 
-        f = [c[0], c[0], c[0], c[0], c[0], c[0], c[0],c[0], c[0], c[0], c[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0]]
+        f = [c[0], c[0], c[0], c[0], c[0], c[0], c[0],c[0], c[0], c[0],c[0], c[0], d[0], d[0], c[0]]
         username = ''.join(f)
         if username in banned[0]:
-            c = random.choices(a)
-            d = random.choices(a) 
-            f = [c[0], c[0], c[0], c[0], c[0], c[0], c[0], c[0], c[0], c[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0], d[0]]
-            username = ''.join(f)
-        else:
-            pass
+         c = random.choices(a)
+         d = random.choices(a) 
+         f = [c[0], c[0], c[0], c[0], c[0], c[0], c[0],c[0], c[0], c[0],c[0], c[0], d[0], d[0], c[0]]
+         username = ''.join(f)
+     else:
+         pass
     return username
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر"))
