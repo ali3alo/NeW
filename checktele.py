28db95ce007c8dec5ad8ab5f61a270dd49759f79
ali3alo <120683286+ali3alo@users.noreply.github.com>
2023-08-29 15:14:58 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -332,18 +332,34 @@ def gen_user(choice):
         else: 
             pass
     if choice == "22":
-        c = d=random.choices(a)
+        c = d =random.choices(a)
         d = random.choices(e)
         s = random.choices(b)
         f =  [c[0], d[0], s[0], s[0], s[0]]
         username = ''.join(f)
         if username in banned[0]:
-            c = d=random.choices(a)
+            c = d =random.choices(a)
             d = random.choices(e)
             s = random.choices(b)
             f =  [c[0], d[0], s[0], s[0], s[0]]
             username = ''.join(f) 
         else: 
+            pass
+    if choice == "23":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(e)
+        f = [c[0], d[0], d[0], d[0], s[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(a)
+            s = random.choices(e)
+            f = [c[0], d[0], d[0], d[0], s[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
             pass
     return username
 
