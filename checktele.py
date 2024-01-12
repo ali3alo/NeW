3b331ce4ba077df4ebbe2fec72a728fea31b803b
ali3alo <120683286+ali3alo@users.noreply.github.com>
2024-01-13 00:28:52 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -285,6 +285,18 @@ def gen_user(choice):
             username = ''.join(f)
         else:
             pass
+    if choice == "20":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], d[0], d[0], c[0], d[0], d[0]]
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], d[0], d[0], c[0], d[0], d[0]]
+            username = ''.join(f)
+        else:
+            pass
     return username
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر"))
